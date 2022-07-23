6c8f0735f35cc25059e8cf40877e473db33b5e9b
Duongld <duongldph15278@fpt.edu.vn>
2022-07-23 12:16:31 +0700

    Update

--- src/pages/Home/index.ts
+++ src/pages/Home/index.ts
@@ -1,6 +1,7 @@
 const HomePage = {
     render: async () => {
-        return /*html*/`<h1>Home page</h1>`
+        return /*html*/`<h1>Home page</h1>
+                        <a href="/admin">Admin Page</a>`   
     }
 }
 
